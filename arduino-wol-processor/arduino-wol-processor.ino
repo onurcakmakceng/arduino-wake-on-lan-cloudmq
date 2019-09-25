596f3f94b61caa6e6c974fe2117b9d56874a3388
Onur Çakmak <onurcakmak95@gmail.com>
2019-09-25 15:01:32 +0300

    Code readability improved

--- arduino-wol-processor/arduino-wol-processor.ino
+++ arduino-wol-processor/arduino-wol-processor.ino
@@ -1,47 +1,46 @@
-
 #include <SPI.h>
 #include <Ethernet.h>
 
 
- // the media access control (ethernet hardware) address for the shield:
+//#TODO: These variables can be 'const', consider
+//--FIELDS--------------------------------------------
+// The media access control (ethernet hardware) address for the shield:
+byte mac[] = { 0x00, 0x00, 0x0B, 0xAA, 0xBB, 0xCC }; 
 
-byte mac[] = { 0x00, 0x00, 0x0B, 0xAA, 0xBB, 0xCC };  
-
-//The broadcast IP of our network
-
+// The broadcast IP of our network
 byte broadCastIp[] = { 255, 255, 255, 255 };
 
-//Mac address of the machine we want to WOL
-
+// Mac address of the machine we want to WOL
 byte remote_MAC_ADD[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 //UDP WOL port
-
 int wolPort = 9;
+//----------------------------------------------------
 
 
-void setup()
-{  
+//--INITIALIZE----------------------------------------
+void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
 
   Serial.println("Arduino is initializing. Please wait...");
 
-  // initialize the ethernet device
-  
-    if (Ethernet.begin(mac) == 0) {
-      Serial.println("Failed to configure Ethernet using DHCP");
-      if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-        Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      } else if (Ethernet.linkStatus() == LinkOFF) {
-        Serial.println("Ethernet cable is not connected.");
-      }
-      // no point in carrying on, so do nothing forevermore:
-      while (true) {
-        delay(1);
-      }
+  // Initialize the ethernet device
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    } else if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
     }
-  
+    
+    // no point in carrying on, so do nothing forevermore:
+    while (true) {
+      delay(1);
+    }
+  }
+
   // start the Ethernet connection:
   Serial.println("Initialize Ethernet with DHCP:");
 
@@ -49,40 +48,38 @@ void setup()
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
 }
+//----------------------------------------------------
 
 
+//--WORKER-THREAD-------------------------------------
+void loop() {
+  if (Serial.read() == 'w') {
+     
+    Serial.println("I'm in");
+    
+    byte magicPacket[102];
+    
+    //#TODO: String operations can be implemented later
+    int i, j, k;
+    
+    // Magic message? or Offset?
+    for (i = 0; i < 6; i++) {
+      magicPacket[i] = 0xFF;
+    }
 
-void loop()
-{
-  if(Serial.read() == 'w'){
-              Serial.println("I'm in");
-              byte magicPacket[102];
+    // Target MAC Address
+    for (j = 0; j < 16; j++) {
+      for (k = 0; k < 6; k++, i++) {
+        magicPacket[i] = remote_MAC_ADD[k];
+      }
+    }
 
-              int i,c1,j=0;
+    EthernetUDP Udp;
 
-              for(i = 0; i < 6; i++,j++){
-
-                  magicPacket[j] = 0xFF;
-
-              }
-
-              for(i = 0; i < 16; i++){
-
-                  for( c1 = 0; c1 < 6; c1++,j++)
-
-                    magicPacket[j] = remote_MAC_ADD[c1];
-
-              }
-              
-              EthernetUDP Udp;
-
-              Udp.begin(wolPort);
-
-              Udp.beginPacket(broadCastIp, wolPort);
-
-              Udp.write(magicPacket, sizeof magicPacket);
-
-              Udp.endPacket();
-
-          }
+    Udp.begin(wolPort);
+    Udp.beginPacket(broadCastIp, wolPort);
+    Udp.write(magicPacket, sizeof magicPacket);
+    Udp.endPacket();
+  }
 }
+//----------------------------------------------------
