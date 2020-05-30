8fb1df6be2d631bbb876a350dcc6da8c0d8023ce
Onur Çakmak <onurcakmak95@gmail.com>
2020-05-30 17:53:57 +0300

    minor fix on statement

--- client/send_magic_packet.py
+++ client/send_magic_packet.py
@@ -10,7 +10,7 @@ data = ''.join(['FF' * 6, mac.replace(':', '') * 16])
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-if sys.version_info[0] < 2.7:
+if sys.version_info[0] <= 2.7:
     # hexadecimal string to byte array in Python ≤ 2.7
     sock.sendto(data.decode("hex"), (sys.argv[1], port))
 else:
