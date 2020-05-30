0a483d43c37a2181109c34548b0c4c4ab0191714
Onur Çakmak <onurcakmak95@gmail.com>
2020-05-30 17:49:41 +0300

    Bug-Fix on Python3 and later versions

--- client/send_magic_packet.py
+++ client/send_magic_packet.py
@@ -9,4 +9,10 @@ mac = sys.argv[2]
 data = ''.join(['FF' * 6, mac.replace(':', '') * 16])
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.sendto(data.decode("hex"), (sys.argv[1], port))
+
+if sys.version_info[0] < 2.7:
+    # hexadecimal string to byte array in Python ≤ 2.7
+    sock.sendto(data.decode("hex"), (sys.argv[1], port))
+else:
+    # hexadecimal string to byte array in Python ≥ 3
+    sock.sendto(bytes.fromhex(data), (sys.argv[1], port))
